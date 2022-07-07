296c6d9bae563278a5c2c0b554805ecb2d6348f5
Juan P. Isaza <jisazamp@gmail.com>
2022-07-06 22:50:02 -0500

    Added the Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
