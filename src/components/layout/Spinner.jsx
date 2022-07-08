02eecaf97e4d8cf4799ba8532e01926845d124d9
Juan P. Isaza <jisazamp@gmail.com>
2022-07-08 16:28:04 -0500

    Added UserItem component

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,7 +1,7 @@
 import '../../styles/spinner.css';
 
 const Spinner = () => {
-  return <div class='loader'>Loading...</div>;
+  return <div className='loader'>Loading...</div>;
 };
 
 export default Spinner;
